5d21934fc976434114ba3fb93fb7d546713332d2
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 00:37:05 +0800

    feat(correction) : add correction service

---
+++ server/src/modules/correction/correction.service.ts
@@ -0,0 +1,39 @@
+import { CorrectionInput } from "./correction.schema";
+import { CorrectionResult, relevanceLabelMap } from "./correction.type";
+import * as CorrectionRepository from "./correction.repository";
+import { generateDummyAIResult } from "../../utils/ai";
+import { NotFoundError } from "../../utils/error";
+
+export async function getAllCorrections(userId: string) {
+  return CorrectionRepository.findAllCorrections(userId);
+}
+
+export async function getCorrectionById(id: string, userId: string): Promise<CorrectionResult> {
+  const correction = await CorrectionRepository.findCorrectionById(id, userId);
+  if (!correction) throw new NotFoundError("Data koreksi tidak ditemukan");
+  return formatCorrectionRelevanceLabel(correction);
+}
+
+export async function createCorrection(userId: string, data: CorrectionInput): Promise<CorrectionResult> {
+  const correction = await CorrectionRepository.createCorrection(userId, data, generateDummyAIResult());
+  return formatCorrectionRelevanceLabel(correction);
+}
+
+export async function updateCorrection(id: string, userId: string, data: CorrectionInput): Promise<CorrectionResult> {
+  const correction = await CorrectionRepository.findCorrectionById(id, userId);
+  if (!correction) throw new NotFoundError("Data koreksi tidak ditemukan");
+
+  const updatedCorrection = await CorrectionRepository.updateCorrection(id, userId, data, generateDummyAIResult());
+  return formatCorrectionRelevanceLabel(updatedCorrection);
+}
+
+export async function deleteCorrection(id: string, userId: string): Promise<void> {
+  const correction = await CorrectionRepository.findCorrectionById(id, userId);
+  if (!correction) throw new NotFoundError("Data koreksi tidak ditemukan");
+  await CorrectionRepository.deleteCorrection(id, userId);
+}
+
+function formatCorrectionRelevanceLabel(correction: CorrectionResult): CorrectionResult {
+  const relevanceLabel = relevanceLabelMap[correction.relevanceLabel] as string;
+  return { ...correction, relevanceLabel };
+}
