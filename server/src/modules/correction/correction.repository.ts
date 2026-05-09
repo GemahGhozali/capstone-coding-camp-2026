3675f8c8a630baf075ed836e0cbfd145e629fef5
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 00:36:50 +0800

    feat(correction) : add correction repository

---
+++ server/src/modules/correction/correction.repository.ts
@@ -0,0 +1,82 @@
+import prisma from "../../libs/prisma";
+import { CorrectionInput } from "./correction.schema";
+import { AIResult } from "./correction.type";
+
+export async function findAllCorrections(userId: string) {
+  return prisma.correction.findMany({
+    where: { userId },
+    select: {
+      id: true,
+      question: true,
+      finalScore: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export async function findCorrectionById(id: string, userId: string) {
+  return prisma.correction.findFirst({
+    where: { id, userId },
+    omit: { userId: true },
+    include: {
+      answerReferences: {
+        omit: { correctionId: true },
+      },
+    },
+  });
+}
+
+export async function createCorrection(userId: string, data: CorrectionInput, aiResult: AIResult) {
+  return prisma.correction.create({
+    data: {
+      userId,
+      question: data.question,
+      gradedAnswer: data.gradedAnswer,
+      finalScore: aiResult.finalScore,
+      similarityScore: aiResult.similarityScore,
+      relevanceLabel: aiResult.relevanceLabel,
+      feedback: aiResult.feedback,
+      answerReferences: {
+        create: data.answerReferences.map((answer) => ({ answer })),
+      },
+    },
+    omit: { userId: true },
+    include: {
+      answerReferences: {
+        omit: { correctionId: true },
+      },
+    },
+  });
+}
+
+export async function updateCorrection(id: string, userId: string, data: CorrectionInput, aiResult: AIResult) {
+  return prisma.correction.update({
+    where: { id, userId },
+    data: {
+      question: data.question,
+      gradedAnswer: data.gradedAnswer,
+      finalScore: aiResult.finalScore,
+      similarityScore: aiResult.similarityScore,
+      relevanceLabel: aiResult.relevanceLabel,
+      feedback: aiResult.feedback,
+      answerReferences: {
+        deleteMany: {},
+        create: data.answerReferences.map((answer) => ({ answer })),
+      },
+    },
+    omit: { userId: true },
+    include: {
+      answerReferences: {
+        omit: { correctionId: true },
+      },
+    },
+  });
+}
+
+export async function deleteCorrection(id: string, userId: string) {
+  return prisma.correction.delete({
+    where: { id, userId },
+  });
+}
