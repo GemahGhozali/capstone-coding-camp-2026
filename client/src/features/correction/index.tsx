daafcab608811adbb437431dfdc63d0d66fbf637
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 09:47:31 +0800

    feat(correction) : add correction types

--- client/src/features/correction/index.tsx
+++ client/src/features/correction/index.tsx
@@ -1,2 +1,4 @@
+export * from "./types/correction.type";
+
 export * from "./pages/CorrectionPage";
 export * from "./pages/CorrectionDetailsPage";
---
+++ client/src/features/correction/types/correction.type.ts
@@ -0,0 +1,27 @@
+export interface CorrectionHistory {
+  id: string;
+  question: string;
+  finalScore: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface AnswerReference {
+  id: string;
+  answer: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CorrectionDetail {
+  id: string;
+  question: string;
+  gradedAnswer: string;
+  finalScore: number;
+  similarityScore: number;
+  relevanceLabel: string;
+  feedback: string;
+  answerReferences: AnswerReference[];
+  createdAt: string;
+  updatedAt: string;
+}
