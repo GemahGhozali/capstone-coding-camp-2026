0c00c4224f9b7e407f2993dfc3c1873d005767ba
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 08:50:52 +0800

    feat(auth) : add schema and layout component export in index.ts file

--- client/src/features/auth/index.tsx
+++ client/src/features/auth/index.tsx
@@ -1,4 +1,9 @@
 export * from "./types/auth.type";
 
+export * from "./schemas/register.schema";
+export * from "./schemas/login.schema";
+
 export * from "./pages/RegisterPage";
 export * from "./pages/LoginPage";
+
+export * from "./components/layouts/AuthLayout";
