4a61a2ee49b12f045bd0f692fb7b1228bb3690dc
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 09:33:10 +0800

    feat(landing-page) : update home page component

--- client/src/features/landing-page/pages/HomePage.tsx
+++ client/src/features/landing-page/pages/HomePage.tsx
@@ -1,3 +1,19 @@
+import AboutSection from "../components/AboutSection";
+import CTASection from "../components/CTASection";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import HowItWorksSection from "../components/HowItWorksSection";
+
 export function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <main className="bg-neutral-50">
+      <HeroSection />
+      <div className="w-full px-4 md:px-6 md:pb-16 lg:pb-25">
+        <HowItWorksSection />
+        <AboutSection />
+        <CTASection />
+      </div>
+      <Footer />
+    </main>
+  );
 }
