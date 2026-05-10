e6ff8fc995bfa90bc08fe38f1a7a22aa582ac812
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-10 10:53:07 +0800

    feat(correction) : add answer references input item component

---
+++ client/src/features/correction/components/AnswerReferencesInputItem.tsx
@@ -0,0 +1,27 @@
+import Input from "@/components/ui/Input";
+import Button from "@/components/ui/Button";
+import NoteIcon from "@/assets/icons/NoteIcon";
+import DeleteIcon from "@/assets/icons/DeleteIcon";
+import PlusIcon from "@/assets/icons/PlusIcon";
+
+interface ReferenceInputItemProps {
+  index: number;
+  isLast: boolean;
+  error?: string;
+  onAdd: () => void;
+  onDelete: (index: number) => void;
+  inputProps: React.InputHTMLAttributes<HTMLInputElement>;
+}
+
+export default function ReferenceInputItem({ index, isLast, error, onAdd, onDelete, inputProps }: ReferenceInputItemProps) {
+  return (
+    <div className="flex gap-4">
+      <Input placeholder={`Tuliskan referensi jawaban ke-${index + 1} disini...`} icon={<NoteIcon className="size-5 text-neutral-500" />} className="grow" error={error} {...inputProps} />
+      {isLast ? (
+        <Button icon={<PlusIcon className="size-5 fill-white" />} iconPosition="only" size="large" color="black" onClick={onAdd} type="button" className="shrink-0" />
+      ) : (
+        <Button icon={<DeleteIcon className="size-5 fill-white" />} iconPosition="only" size="large" color="red" type="button" onClick={() => onDelete(index)} className="shrink-0" />
+      )}
+    </div>
+  );
+}
