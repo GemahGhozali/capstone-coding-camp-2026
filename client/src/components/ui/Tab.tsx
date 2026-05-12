b205651015732570d382c6e89e40a63d4fbb9fc6
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-12 22:08:34 +0800

    fix(component) : add hover style to tab component

--- client/src/components/ui/Tab.tsx
+++ client/src/components/ui/Tab.tsx
@@ -8,7 +8,7 @@ interface TabProps {
 }
 
 export default function Tab({ isActive = false, className = "", onClick, children }: TabProps) {
-  const tabClassStyle = `rounded-4xl cursor-pointer text-caption px-4 py-2 border ${isActive ? "border-blue-600 bg-blue-100 text-blue-600 font-semibold" : "border-neutral-300 bg-white text-black"} ${className}`;
+  const tabClassStyle = `duration-300 transition-colors rounded-4xl cursor-pointer text-caption px-4 py-2 border ${isActive ? "border-blue-600 bg-blue-100 text-blue-600 font-semibold" : "border-neutral-300 bg-white text-black hover:bg-neutral-100"} ${className}`;
 
   return (
     <button type="button" onClick={onClick} className={tabClassStyle}>
