80f4a6a1a09d243fb99452b13ade6a3b0f034bc7
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-13 15:24:02 +0800

    feat(correction) : add correction layout component export to index.ts file

--- client/src/features/correction/index.tsx
+++ client/src/features/correction/index.tsx
@@ -9,3 +9,4 @@ export * from "./schemas/correction.schema";
 export * from "./pages/CorrectionPage";
 export * from "./pages/CorrectionDetailsPage";
 export * from "./pages/CorrectionNotFoundPage";
+export * from "./components/layouts/CorrectionLayout";
