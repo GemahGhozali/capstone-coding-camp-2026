8cc5236e4460428a417a7a8a4fbbffc9607fd151
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-28 06:18:42 +0800

    chore(server) : remove host in entry point file for production adjustment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import app from "./app";
 
-const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`✅ Server is running on http://${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server is running on port ${PORT}`));
