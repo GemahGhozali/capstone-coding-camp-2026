d8dcc6c0e0937ff20cb40bcd610813c322ddaa23
Gemah Ghozali <gemahghozali@gmail.com>
2026-05-29 14:11:00 +0800

    fix(server) : remove similarityScore field in correction schema and run migration changes

---
+++ server/prisma/migrations/20260529060922_remove_similarity_score_field_from_correction_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `similarityScore` on the `Correction` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Correction" DROP COLUMN "similarityScore";
